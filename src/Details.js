d70b34f8b59708c7a84e9c2fbe7bc77b1ec32029
opagani <opagani@zillowgroup.com>
2018-08-28 08:17:40 -0700

    Adding Async UI

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pf from "petfinder-client";
 import { navigate } from "@reach/router";
+import Carousel from "./Carousel";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -45,10 +46,11 @@ class Details extends React.Component {
       return <h1>loading …</h1>;
     }
 
-    const { name, animal, breed, location, description } = this.state;
+    const { name, animal, breed, location, description, media } = this.state;
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>
